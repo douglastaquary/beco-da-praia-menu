be2fbb94242383d4a692927df53e12a2401f66e6
Douglas Taquary <douglastaquary@gmail.com>
2024-09-15 14:29:15 -0300

    Ajuste da lista de produtos. Customização dos componentes. Navegacão para tela de detalhes.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  
 })
 export class ProductListComponent {
   products = [...products];
